d90fe099cba8b7f817ef9111323e11c4fdc1bc4c
Marcel van der Meer <marcelmeer@gmail.com>
2024-08-22 17:45:38 +0200

    updated page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,80 @@
 
 <div class="container">
 
-<div class="main-container-left">
+<div class="container-left">
+
     <div class="formchecks">
 
-    </div>
+    <form action='' id='formCheckboxes' method= 'get'>
+
+<br>
+<strong>Geslacht<br></strong>
+
+<div>
+<input checked type="checkbox"/>Man
+</div>
+<div>
+<input checked type="checkbox"/>Vrouw
+</div>
+<br>
+<strong>Leeftijd<br></strong> 
+<br>
+<strong>Aan boord gegaan in<br></strong>
+<div>
+<input checked type="checkbox"/>Belfast
+</div>
+<div>
+<input checked type="checkbox"/>Cherbourg
+</div>
+<div>
+<input checked type="checkbox"/>Queenstown
+</div>
+<div>
+<input checked type="checkbox"/>Southampton
+</div>
+<br>
+
+<strong>Klasse<br></strong>
+<div>
+<input checked type="checkbox"/>1e
+</div>
+<div>
+<input checked type="checkbox"/>2e
+</div>
+<div>
+<input checked type="checkbox"/>3e
+</div>
+ <br>
+<strong>Overleefd / omgekomen<br></strong>
+<div>
+<input checked type="checkbox"/>Overleefd
+</div>
+<div>
+<input checked type="checkbox"/>Omgekomen
+</div>
+<br>
+
+<button class='inline' id='btn-filter' name="checkBtn">Pas toe</button>
+<button id='btn-filter_red' name="reset">Reset</button>
+
+<div class='formname'> 
+
+<form action='' method='get'> 
+
+<strong>Zoek op een naam<p></strong>
+
+<input name='enteredName' id='input_name' placeholder='Voer naam in'>
+<br>
+<br>
+
+<button id='btn-filter' class='margin_left' name='searchName'>Zoek</button> 
+
+<hr>
+
+</div>
+</form>
+
+</div>
 </div>
 
 <div class="main-container-right">
